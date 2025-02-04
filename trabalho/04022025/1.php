be9cbd990216dedcc49648f7c933757f564e9a60
Kira_Chan <157720938+Kirachan1@users.noreply.github.com>
2025-02-04 11:53:01 +0000

    Create 1.php

---
+++ trabalho/04022025/1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> TRABALHO DE RC 1</title>
+    <style>
+        div {
+            width: 150px;
+            font-family: Verdana;
+            background-color: #C8F7C5;
+            box-shadow: 10 px 10px 5px #888888;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <table border="1">
+            <tr>
+                <td><b>Números</b></td>
+                <td><b>Cubos</b></td>
+            </tr>
+            <?php
+            for($i=1;$1<=5;$i++){
+                $q=$i=$i=$i;
+                echo "<td> $q><td></tr>";
+            }
+            ?>
+        </table>
+    </div>
+</body>
+</html>
